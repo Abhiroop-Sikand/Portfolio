7e01b80a8980ef85719812bac4e2e0ba53327e6c
Abhiroop Sikand <72399460+Abhiroop-Sikand@users.noreply.github.com>
2023-10-17 16:56:48 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+var typed = new Typed(".aniText", {
+    strings:["a Software Engineer", "a Student", "Your Best Decision"],
+    typeSpeed:100,
+    backSpeed:70,
+    backDelay: 1100,
+    loop: true
+});
+
+let index = 0;
+displayImages();
+function displayImages() {
+  let i;
+  const images = document.getElementsByClassName("about-img");
+  for (i = 0; i < images.length; i++) {
+    images[i].style.display = "none";
+  }
+  index++;
+  if (index > images.length) {
+    index = 1;
+  }
+  images[index-1].style.display = "block";
+  setTimeout(displayImages, 2000); 
+}
